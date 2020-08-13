1f4fe2f17c67162d358b643ea345dc640ecfb5e5
ShaoxinGu <853674765@qq.com>
2020-08-14 02:27:07 +0800

    modify splash

--- game/script/common/splash_screen.gd
+++ game/script/common/splash_screen.gd
@@ -1,34 +1,41 @@
 extends CenterContainer
 
 onready var animation_player : AnimationPlayer = $AnimationPlayer
-onready var frame_anim : AnimatedSprite = $Panel/GamejamLogo
+onready var logo_anim : AnimatedSprite = $Panel/GamejamLogo
+onready var studio_anim : AnimatedSprite = $Panel/StudioLogo
 signal done
 
 func _ready():
 	animation_player.connect("animation_finished", self, "animation_player_finished")
-	frame_anim.connect("animation_finished", self, "frame_anim_finished")
+	logo_anim.connect("animation_finished", self, "logo_anim_finished")
+	studio_anim.connect("animation_finished", self, "studio_anim_finished")
 	animation_player.play("show_godot")
 	
 func _input(event):
 	if event is InputEventKey or event is InputEventMouseButton or event is InputEventJoypadButton:
 		if event.pressed and animation_player.is_playing():
 			animation_player.seek(max(4.0, animation_player.current_animation_position), true)
-		elif event.pressed and frame_anim.is_playing():
-			frame_anim.stop()
-			frame_anim.emit_signal("animation_finished")
+		elif event.pressed and logo_anim.is_playing():
+			logo_anim.stop()
+			logo_anim.emit_signal("animation_finished")
+		elif event.pressed and studio_anim.is_playing():
+			studio_anim.stop()
+			studio_anim.emit_signal("animation_finished")
 	get_tree().set_input_as_handled()
 
 func animation_player_finished(anim: String):
 	if anim == "show_godot":
-		frame_anim.show()
-		frame_anim.animation = "show_logo"
-		frame_anim.play()
-	elif anim == "show_studio":
-		print("finished")
-		emit_signal("done")
-		queue_free()
-		#TODO switch to main scene	--醉醉
+		logo_anim.show()
+		logo_anim.animation = "show_logo"
+		logo_anim.play()
 
-func frame_anim_finished():
-	animation_player.play("show_studio")
-	frame_anim.hide()
+func logo_anim_finished():
+	studio_anim.show()
+	studio_anim.animation = "show_studio"
+	studio_anim.play()
+	
+func studio_anim_finished():
+	print("finished")
+	emit_signal("done")
+	queue_free()
+	#TODO switch to main scene	--醉醉

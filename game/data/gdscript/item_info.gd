6b50916da17fafbb7c37d8d6ed8f3af444c1adee
vmjcv <vmjcv666@gmail.com>
2020-08-16 13:13:18 +0800

    add

--- game/data/gdscript/item_info.gd
+++ game/data/gdscript/item_info.gd
@@ -13,5 +13,23 @@ class item_infoData:
 static func load_configs():
 	return [
 		item_infoData.new(1, "这是妈妈给我的钢笔哟", "钢笔"),
-		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "钢笔_1_2"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "红花"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "黄花"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "奖状"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "蓝花"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "通知书"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "纸"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "草戒指"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "负面情绪"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "干燥剂"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "钢笔"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "花"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "花束"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "花照片"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "家规"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "奖状"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "结婚照"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "通知书"),
+		item_infoData.new(1, "这是一周目第二关卡妈妈送给我的礼物哦", "钻戒"),
+		
 	]

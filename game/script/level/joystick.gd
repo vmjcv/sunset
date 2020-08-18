a11435f71bb45937258bc4506121564bb1dec49b
ShaoxinGu <853674765@qq.com>
2020-08-19 00:12:50 +0800

    modify joystick

--- game/script/level/joystick.gd
+++ game/script/level/joystick.gd
@@ -1,6 +1,7 @@
-extends Sprite
+extends TouchScreenButton
 
-var maxLen = 60
+var offset = Vector2(180,180)
+var maxLen = 180
 var threshold = 30
 var onDraging = -1
 var tween
@@ -14,17 +15,17 @@ func _ready():
 
 func _input(event):
 	if event is InputEventScreenDrag or (event is InputEventScreenTouch and event.is_pressed()):
-		var mouse_pos = (event.position - self.global_position).length()
+		var mouse_pos = (event.position - offset - self.global_position).length()
 		if mouse_pos <= maxLen or event.get_index() == onDraging:
 			onDraging = event.get_index()
 			$point.set_global_position(event.position)
-			if get_point_pos().length() > maxLen:
-				$point.set_position(get_point_pos().normalized()*maxLen)
+			if (get_point_pos() - offset).length() > maxLen:
+				$point.set_position((get_point_pos() - offset).normalized()*maxLen + offset)
 	if event is InputEventScreenTouch and !event.is_pressed():
 		if event.get_index() == onDraging:
 			set_center()
 			onDraging = -1
-			var pos = get_point_pos()
+			var pos = get_point_pos() - offset
 			if pos.length() > threshold:
 				if abs(pos.x) > abs(pos.y):
 					if pos.x < 0:
@@ -42,7 +43,7 @@ func get_point_pos():
 	return $point.position
 
 func set_center():
-	tween.interpolate_property($point, "position", get_point_pos(), Vector2(0,0), 0.1, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+	tween.interpolate_property($point, "position", get_point_pos(), offset, 0.1, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 	tween.start()
 	
 
